028a95c21c27c972c516f277a3cb869916984703
javacook <javacook@gmx.de>
2017-05-19 06:25:15 +0200

    initial

---
+++ src/main/java/com/iks/dddschach/rest/providers/ErrorCode.java
@@ -0,0 +1,12 @@
+package com.iks.dddschach.rest.providers;
+
+/**
+ * Created by vollmer on 02.05.17.
+ */
+public enum ErrorCode {
+
+    TIMEOUT,
+    INVALID_MOVE;
+
+    public final static String ERROR_CODE_KEY = "error code";
+}
