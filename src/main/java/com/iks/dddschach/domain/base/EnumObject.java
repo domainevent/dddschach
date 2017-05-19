a5be9d5c9f32e631d502ffc45fb9cb055f780303
javacook <javacook@gmx.de>
2017-05-19 08:47:29 +0200

    Enum-Objekt brauch den generischen Enum-Typ E ja gar nicht

--- src/main/java/com/iks/dddschach/domain/base/EnumObject.java
+++ src/main/java/com/iks/dddschach/domain/base/EnumObject.java
@@ -1,25 +1,27 @@
 package com.iks.dddschach.domain.base;
 
-import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonValue;
 
 import javax.xml.bind.annotation.XmlEnum;
 
 
+/**
+ * Enums sollten diese Interface implementieren.
+ * @param <M> Klasse, in die die Enum-Literale transformiert werden (i.d.R. String)
+ */
 @XmlEnum
-public interface EnumObject<E, M> {
+public interface EnumObject<M> {
 
     /**
      * Transformiert von der Domain-Bezeichnung (ubiquitäre Sprache) in die der technischen
      * Transportschicht (hier JSON). Jedes Enum sollte (zumindest im Client) folgende
-     * statische Methode zum Unmarshallen enthalten:
+     * statische Methode zum Unmarshallen enthalten, wobei E für den Enum-Typ steht:
      * <pre>
      *   @JsonCreator
-     *   static <E> E unmarshal(M encoded);
+     *   static <E extends Enum> E unmarshal(M encoded);
      * </pre>
      * @return der kodierte Enum-Wert
      */
     @JsonValue
     M marshal();
-
 }
