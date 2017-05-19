630f8a125b81dfb434833b89564ca6b8eb2ba945
javacook <javacook@gmx.de>
2017-05-19 08:52:48 +0200

    Kommentierungen

--- src/main/java/com/iks/dddschach/domain/base/EntityObject.java
+++ src/main/java/com/iks/dddschach/domain/base/EntityObject.java
@@ -4,7 +4,7 @@ import javax.xml.bind.annotation.XmlType;
 
 
 /**
- * Marker für Entity Objects
+ * Marker für Entity Objects. Nicht jedes Entity Object besitzt eine explizite Id.
  */
 @XmlType
 public abstract class EntityObject {
