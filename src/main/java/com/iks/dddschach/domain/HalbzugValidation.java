bd67289d3cfdac619231d64d6c383b8b89684940
javacook <javacook@gmx.de>
2017-05-19 06:23:25 +0200

    beta implementation of HalbzugValidator

--- src/main/java/com/iks/dddschach/domain/HalbzugValidation.java
+++ src/main/java/com/iks/dddschach/domain/HalbzugValidation.java
@@ -11,14 +11,31 @@ import java.util.Optional;
  */
 public class HalbzugValidation implements DomainService {
 
-    public final class ValidationResult {}
+    public final class ValidationResult {
+        public boolean valid;
+
+        public ValidationResult(boolean valid) {
+            this.valid = valid;
+        }
+    }
 
     public ValidationResult validiere(
             Halbzug zuPruefen,
             HalbzugHistorie zugHistorie,
             Spielbrett aktSpielbrett) {
 
-        return null;
+
+        final Spielfigur schachfigurAnFrom = aktSpielbrett.getSchachfigurAnPosition(zuPruefen.from);
+        if (schachfigurAnFrom == null) {
+            return new ValidationResult(false);
+        }
+
+        if (schachfigurAnFrom.color.ordinal() != zugHistorie.size() % 2) {
+            return new ValidationResult(false);
+        }
+
+        return new ValidationResult(true);
+
     }
 
 }
