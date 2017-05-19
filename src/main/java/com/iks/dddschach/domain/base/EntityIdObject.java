c71139b9a64f1f39bdf57525fa37e3e2e3141a7e
javacook <javacook@gmx.de>
2017-05-19 08:19:57 +0200

    Renamed: EntityObject -> EntityIdObject

--- src/main/java/com/iks/dddschach/domain/base/EntityIdObject.java
+++ src/main/java/com/iks/dddschach/domain/base/EntityIdObject.java
@@ -7,16 +7,16 @@ import javax.xml.bind.annotation.XmlType;
  * Created by vollmer on 17.05.17.
  */
 @XmlType
-public abstract class EntityObject<I> {
+public abstract class EntityIdObject<I> {
 
     protected final I id;
 
-    public EntityObject() {
+    public EntityIdObject() {
         this(null);
         throw new IllegalStateException("This constructor must not be called");
     }
 
-    protected EntityObject(I id) {
+    protected EntityIdObject(I id) {
         this.id = id;
     }
 
@@ -28,7 +28,7 @@ public abstract class EntityObject<I> {
     public boolean equals(Object o) {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
-        EntityObject<?> that = (EntityObject<?>) o;
+        EntityIdObject<?> that = (EntityIdObject<?>) o;
         return id.equals(that.id);
     }
     @Override
