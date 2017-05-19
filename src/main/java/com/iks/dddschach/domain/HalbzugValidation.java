3078a4a8da6d2064f5a1cbbd69cf5f2f2ea7e654
javacook <javacook@gmx.de>
2017-05-19 09:07:13 +0200

    Kommntierung

--- src/main/java/com/iks/dddschach/domain/HalbzugValidation.java
+++ src/main/java/com/iks/dddschach/domain/HalbzugValidation.java
@@ -3,16 +3,26 @@ package com.iks.dddschach.domain;
 import com.iks.dddschach.domain.base.DomainService;
 
 import java.util.List;
-import java.util.Optional;
 
 
 /**
- * Created by vollmer on 17.05.17.
+ * Dieser Service dient dazu, eine Überprüfung eines Halbzuges auf Gültigkeit vorzunehemn.
+ * Der Einfachheit halber, wird in dieser Implementierung lediglich getestet, ob sich überhaupt
+ * eine Figur auf der Startposition des Halbzuges befindet und ob diese die richtig Farbe
+ * hat - Die Spieler "Weiß" und "Schwarz" müssen sich abwechseln.
  */
 public class HalbzugValidation implements DomainService {
 
     public final class ValidationResult {}
 
+
+    /**
+     *
+     * @param zuPruefen
+     * @param zugHistorie
+     * @param aktSpielbrett
+     * @return
+     */
     public ValidationResult validiere(
             Halbzug zuPruefen,
             List<Halbzug> zugHistorie,
