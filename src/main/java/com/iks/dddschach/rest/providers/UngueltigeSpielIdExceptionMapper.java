e215df935a86d7064ca1477ef73806d511ce4ee4
javacook <javacook@gmx.de>
2017-05-21 10:57:38 +0200

    Behandlung von UngueltigeSpielIdException hinzugefügt
    (cherry picked from commit 3f26ab2)

---
+++ src/main/java/com/iks/dddschach/rest/providers/UngueltigeSpielIdExceptionMapper.java
@@ -0,0 +1,28 @@
+package com.iks.dddschach.rest.providers;
+
+
+import com.iks.dddschach.api.SchachpartieApi;
+import com.iks.dddschach.api.SchachpartieApi.UngueltigeSpielIdException;
+import com.iks.dddschach.api.SchachpartieApi.UngueltigerHalbzugException;
+
+import javax.ws.rs.core.Response;
+import javax.ws.rs.ext.ExceptionMapper;
+import javax.ws.rs.ext.Provider;
+import java.util.HashMap;
+import java.util.Map;
+
+
+/**
+ * Created by vollmer on 19.05.17.
+ */
+@Provider
+public class UngueltigeSpielIdExceptionMapper implements ExceptionMapper<UngueltigeSpielIdException> {
+
+    @Override
+    public Response toResponse(UngueltigeSpielIdException exception) {
+        Map<String, Object> json = new HashMap<>();
+        json.put(ErrorCode.ERROR_CODE_KEY, ErrorCode.INVALID_MOVE);
+        json.put(ErrorCode.INVALID_MOVE.name(), exception.spielId.toString());
+        return Response.status(422).entity(json).build();
+    }
+}
