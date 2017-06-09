b1d381f61fac9065c5e5a4dd88a36c8eb3fd1fb3
javacook <javacook@gmx.de>
2017-06-09 21:59:07 +0200

    initial

---
+++ src/main/java/com/iks/dddschach/domain/validation/RochadenCheck.java
@@ -0,0 +1,97 @@
+package com.iks.dddschach.domain.validation;
+
+import com.iks.dddschach.domain.Halbzug;
+import com.iks.dddschach.domain.Position;
+import com.iks.dddschach.domain.Spielbrett;
+import com.iks.dddschach.domain.Spielfigur;
+import com.iks.dddschach.domain.Spielfigur.FigurenTyp;
+
+import java.util.*;
+import java.util.stream.Collectors;
+
+import static com.iks.dddschach.domain.Position.Spalte.*;
+import static com.iks.dddschach.domain.Position.Zeile._1;
+import static com.iks.dddschach.domain.Position.Zeile._8;
+
+
+public class RochadenCheck implements HalbzugValidation {
+
+	final static FreieBahnCheck FREIE_BAHN_CHECK = new FreieBahnCheck();
+
+	final static Position UR_POSITION_KOENIG_WEISS        = new Position(E,_1);
+    final static Position UR_POSITION_KOENIG_SCHWARZ      = new Position(E,_8);
+    final static Position UR_POSITION_TURM_LINKS_WEISS    = new Position(A,_1);
+    final static Position UR_POSITION_TURM_RECHTS_WEISS   = new Position(H,_1);
+    final static Position UR_POSITION_TURM_LINKS_SCHWARZ  = new Position(A,_8);
+    final static Position UR_POSITION_TURM_RECHTS_SCHWARZ = new Position(H,_8);
+
+	final static Halbzug ROCHADE_HALBZUG_KOENIG_WEISS_GROSS   = new Halbzug(UR_POSITION_KOENIG_WEISS, new Position(C,_1));
+    final static Halbzug ROCHADE_HALBZUG_KOENIG_WEISS_KLEIN   = new Halbzug(UR_POSITION_KOENIG_WEISS, new Position(G,_1));
+    final static Halbzug ROCHADE_HALBZUG_KOENIG_SCHWARZ_GROSS = new Halbzug(UR_POSITION_KOENIG_SCHWARZ, new Position(C,_8));
+    final static Halbzug ROCHADE_HALBZUG_KOENIG_SCHWARZ_KLEIN = new Halbzug(UR_POSITION_KOENIG_SCHWARZ, new Position(G,_8));
+
+    final static Halbzug ROCHADE_HALBZUG_TURM_WEISS_GROSS   = new Halbzug(UR_POSITION_TURM_LINKS_WEISS, new Position(D,_1));
+    final static Halbzug ROCHADE_HALBZUG_TURM_WEISS_KLEIN   = new Halbzug(UR_POSITION_TURM_RECHTS_WEISS, new Position(F,_1));
+    final static Halbzug ROCHADE_HALBZUG_TURM_SCHWARZ_GROSS = new Halbzug(UR_POSITION_TURM_LINKS_SCHWARZ, new Position(D,_8));
+    final static Halbzug ROCHADE_HALBZUG_TURM_SCHWARZ_KLEIN = new Halbzug(UR_POSITION_TURM_RECHTS_SCHWARZ, new Position(F,_8));
+
+    final static Map<Halbzug, Halbzug> ZUGEHOERIGE_TURM_HALBZEUGE = new HashMap<Halbzug, Halbzug>() {{
+        put(ROCHADE_HALBZUG_KOENIG_WEISS_GROSS,   ROCHADE_HALBZUG_TURM_WEISS_GROSS);
+        put(ROCHADE_HALBZUG_KOENIG_WEISS_KLEIN,   ROCHADE_HALBZUG_TURM_WEISS_KLEIN);
+        put(ROCHADE_HALBZUG_KOENIG_SCHWARZ_GROSS, ROCHADE_HALBZUG_TURM_SCHWARZ_GROSS);
+        put(ROCHADE_HALBZUG_KOENIG_SCHWARZ_KLEIN, ROCHADE_HALBZUG_TURM_SCHWARZ_KLEIN);
+    }};
+
+    final static Set<Halbzug> GUELTIGE_ROCHADEN_HALBZEUGE = ZUGEHOERIGE_TURM_HALBZEUGE.keySet();
+
+
+    public static class RochadenCheckResult extends ValidationResult {
+        public final Halbzug turmHalbZug;
+
+        public RochadenCheckResult(Zugregel verletzteZugregel) {
+            super(false, verletzteZugregel);
+            turmHalbZug = null;
+        }
+
+        public RochadenCheckResult(Halbzug turmHalbZug) {
+            super(true, null);
+            this.turmHalbZug = turmHalbZug;
+        }
+    }
+
+
+	@Override
+	public RochadenCheckResult validiere(Halbzug halbzug, List<Halbzug> zugHistorie, Spielbrett spielbrett) {
+        Objects.requireNonNull(halbzug, "Argument halbzug is null");
+        Objects.requireNonNull(spielbrett, "Argument spielbrett is null");
+        Objects.requireNonNull(zugHistorie, "Argument zugHistorie is null");
+
+        Spielfigur zugFigur = spielbrett.getSchachfigurAnPosition(halbzug.from);
+        Objects.requireNonNull(zugFigur, "There is no figure on " + halbzug.from);
+
+        final Set<Position> startPositionen = zugHistorie.stream().map(hz -> hz.from).collect(Collectors.toSet());
+        final Halbzug zugehoerigerTurmHalbzug = ZUGEHOERIGE_TURM_HALBZEUGE.get(halbzug);
+
+        if (zugFigur.figure != FigurenTyp.KOENIG) {
+            return new RochadenCheckResult(Zugregel.HALBZUG_IST_KEIN_ROCHADE);
+        }
+        if (!GUELTIGE_ROCHADEN_HALBZEUGE.contains(halbzug)) {
+            return new RochadenCheckResult(Zugregel.HALBZUG_IST_KEIN_ROCHADE);
+        }
+        // Freie Bahn vom König zum Turm checken:
+        final Halbzug halbzugVonKoenigZuTurm = new Halbzug(halbzug.from, zugehoerigerTurmHalbzug.from);
+        final ValidationResult freieBahnCheckResult =
+                FREIE_BAHN_CHECK.validiere(halbzugVonKoenigZuTurm, zugHistorie, spielbrett);
+        if (!freieBahnCheckResult.gueltig) {
+            return new RochadenCheckResult(freieBahnCheckResult.verletzteZugregel);
+        }
+        if (startPositionen.contains(halbzug.from)) {
+            return new RochadenCheckResult(Zugregel.ROCHADE_KOENIG_WURDE_BEREITS_BEWEGT);
+        }
+        if (startPositionen.contains(zugehoerigerTurmHalbzug.from)) {
+            return new RochadenCheckResult(Zugregel.ROCHADE_TURM_WURDE_BEREITS_BEWEGT);
+        }
+        return new RochadenCheckResult(zugehoerigerTurmHalbzug);
+    }
+
+}
