457eb9854031a88de340f4e40f089613795acc9d
javacook <javacook@gmx.de>
2017-06-17 08:56:57 +0200

    Matt-Patt-Regel eingeführt

---
+++ src/main/java/com/iks/dddschach/domain/validation/PattMattCheck.java
@@ -0,0 +1,52 @@
+package com.iks.dddschach.domain.validation;
+
+import com.iks.dddschach.domain.*;
+
+import java.util.List;
+import java.util.Objects;
+
+import static com.iks.dddschach.domain.validation.PattMattCheck.PattMatt.MATT;
+import static com.iks.dddschach.domain.validation.PattMattCheck.PattMatt.NONE;
+import static com.iks.dddschach.domain.validation.PattMattCheck.PattMatt.PATT;
+
+
+/**
+ * Checkt, ob sich der ziehende Spieler nach seinem Halbzug (noch) im Schach befindet
+ * @author javacook
+ */
+public class PattMattCheck {
+
+    private final static GesamtValidator VALIDATOR = new GesamtValidator();
+
+    public enum PattMatt {
+        PATT, MATT, NONE
+    }
+
+	/**
+	 */
+    public PattMatt analysiere(final List<Halbzug> halbzugHistorie, final Spielbrett spielbrett) {
+        Objects.requireNonNull(spielbrett, "Argument spielbrett is null");
+        final Farbe spielerFarbe = ValidationUtils.spielerFarbe(halbzugHistorie.size());
+
+        // Gehe all meine Figure durch und schauen, ob ich bei jedem gültigen Zug im Schach
+        // steht
+        for (Position meinePosition : spielbrett.getPositionenMitFarbe(spielerFarbe)) {
+            for (Position moeglichesZiel : spielbrett.getAllePositionen()) {
+                final Halbzug halbzug = new Halbzug(meinePosition, moeglichesZiel);
+                final HalbzugValidation.ValidationResult validationResult =
+                        VALIDATOR.validiere(halbzug, halbzugHistorie, spielbrett);
+
+                if (validationResult.gueltig) {
+                    System.out.println(spielbrett);
+                    System.out.println("Halbzug"+ halbzug);
+                    return NONE;
+                }
+            }
+        }
+        final Position koenigsPosition = spielbrett.sucheKoenigsPosition(spielerFarbe);
+        final Halbzug dummyHalbzug = new Halbzug(koenigsPosition, koenigsPosition);
+        return VALIDATOR.validiere(dummyHalbzug, halbzugHistorie, spielbrett).gueltig?
+                PATT : MATT;
+    }
+
+}
