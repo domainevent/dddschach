4a025b3f71eefc09611ec0c6ffb568cfe06f8c99
javacook <javacook@gmx.de>
2017-07-05 20:55:01 +0200

    Tests hinzugefügt

--- src/test/java/com/iks/dddschach/rest/DDDSchachIT.java
+++ src/test/java/com/iks/dddschach/rest/DDDSchachIT.java
@@ -1,11 +1,12 @@
 package com.iks.dddschach.rest;
 
+import com.iks.dddschach.api.SchachpartieApi;
+import com.iks.dddschach.domain.SpielId;
+import com.iks.dddschach.domain.Spielbrett;
+import com.iks.dddschach.domain.SpielbrettFactory;
 import org.junit.Assert;
-import org.junit.BeforeClass;
 import org.junit.Test;
 
-import javax.ws.rs.client.ClientBuilder;
-import javax.ws.rs.client.WebTarget;
 import javax.ws.rs.core.Response;
 
 
@@ -14,26 +15,73 @@ import javax.ws.rs.core.Response;
  */
 public class DDDSchachIT {
 
-    private static String dockerHost;
-    private static String dockerPort;
-    private static WebTarget webTarget;
-
-    @BeforeClass
-    public static void init() {
-        String host = System.getProperty("docker.container.ip");
-        String port = System.getProperty("docker.container.port");
-        dockerHost = (host == null)? "localhost" : host;
-        dockerPort = (port == null)? "8080" : port;
-        webTarget = ClientBuilder.newClient()
-                .target("http://localhost:" + dockerPort + "/dddschach/api");
-    }
 
     @Test
     public void isAlive() {
-        final String property = System.getProperty("docker.container.ip");
-        System.out.println("Hallo Test: " + dockerHost + ":" + dockerPort);
-        final Response response = webTarget.path("isalive").request().get();
-        Assert.assertEquals(200, response.getStatus());
+        final RestServiceClient client = new RestServiceClient();
+        final String date = client.isAlive();
+        Assert.assertNotNull(date);
+    }
+
+    @Test
+    public void neuesSpielGueltigeEingabe() {
+        System.out.println("Test: neuesSpielGueltigeEingabe");
+        final String gueltigerVermerk = makeStringOfLength(100);
+
+        final RestServiceClient client = new RestServiceClient();
+        final SpielId spielId = client.neuesSpiel(gueltigerVermerk);
+        Assert.assertNotNull(spielId.id);
+    }
+
+    @Test
+    public void neuesSpielUngueltigeEingabe() {
+        System.out.println("Test: neuesSpielUngueltigeEingabe");
+        final RestServiceClient client = new RestServiceClient();
+        final String ungueltigerVermerk = makeStringOfLength(101);
+        try {
+            client.neuesSpiel(ungueltigerVermerk);
+            Assert.fail("Expected RestCallFailedException not occured");
+        }
+        catch (RestServiceClient.RestCallFailedException e) {
+            Assert.assertEquals(400, e.statusCode);
+        }
+    }
+
+
+    @Test
+    public void spielBrettUngueltigeSpielId() {
+        System.out.println("Test: spielBrettUngueltigeSpielId");
+        final RestServiceClient client = new RestServiceClient();
+        try {
+            client.spielbrett("", "", null);
+            Assert.fail("Expected UngueltigeSpielIdException not occured");
+        }
+        catch (SchachpartieApi.UngueltigeSpielIdException e) {
+            // expected case
+        }
+    }
+
+    @Test
+    public void spielBrettGueltigeSpielId() {
+        System.out.println("Test: spielBrettGueltigeSpielId");
+        final RestServiceClient client = new RestServiceClient();
+        try {
+            final SpielId spielId = client.neuesSpiel("Vermerk");
+            final Response resp = client.spielbrett(spielId.id, "Test", null);
+            Assert.assertEquals(200, resp.getStatus());
+            final Spielbrett spielbrett = resp.readEntity(Spielbrett.class);
+            Assert.assertEquals(SpielbrettFactory.createInitialesSpielbrett(), spielbrett);
+        }
+        catch (SchachpartieApi.UngueltigeSpielIdException e) {
+            Assert.fail(e.toString());
+        }
+    }
+
+
+    private static String makeStringOfLength(int n) {
+        String result = "";
+        for (int i = 0; i < n; i++) result += (char)i;
+        return result;
     }
 
 }
