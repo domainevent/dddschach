33af09d85cf4b48fec96f44e73b06bb1f2bf6e6c
javacook <javacook@gmx.de>
2017-07-05 23:05:44 +0200

    Zugtests hinzugefügt

--- src/test/java/com/iks/dddschach/rest/DDDSchachIT.java
+++ src/test/java/com/iks/dddschach/rest/DDDSchachIT.java
@@ -1,13 +1,19 @@
 package com.iks.dddschach.rest;
 
 import com.iks.dddschach.api.SchachpartieApi;
-import com.iks.dddschach.domain.SpielId;
-import com.iks.dddschach.domain.Spielbrett;
-import com.iks.dddschach.domain.SpielbrettFactory;
+import com.iks.dddschach.domain.*;
+import com.iks.dddschach.domain.validation.Zugregel;
 import org.junit.Assert;
 import org.junit.Test;
 
 import javax.ws.rs.core.Response;
+
+import java.text.ParseException;
+
+import static com.iks.dddschach.domain.Position.Spalte.E;
+import static com.iks.dddschach.domain.Position.Zeile._2;
+import static com.iks.dddschach.domain.Position.Zeile._4;
+import static com.iks.dddschach.domain.SpielbrettFactory.createInitialesSpielbrett;
 
 
 /**
@@ -15,6 +21,7 @@ import javax.ws.rs.core.Response;
  */
 public class DDDSchachIT {
 
+    public final static String CR = System.lineSeparator();
 
     @Test
     public void isAlive() {
@@ -70,9 +77,90 @@ public class DDDSchachIT {
             final Response resp = client.spielbrett(spielId.id, "Test", null);
             Assert.assertEquals(200, resp.getStatus());
             final Spielbrett spielbrett = resp.readEntity(Spielbrett.class);
-            Assert.assertEquals(SpielbrettFactory.createInitialesSpielbrett(), spielbrett);
+            Assert.assertEquals(createInitialesSpielbrett(), spielbrett);
         }
         catch (SchachpartieApi.UngueltigeSpielIdException e) {
+            Assert.fail(e.toString());
+        }
+    }
+
+    @Test
+    public void fuehreGueltigenHalbzugAus() {
+        System.out.println("Test: fuehreGueltigenHalbzugAus");
+        final RestServiceClient client = new RestServiceClient();
+        try {
+            final SpielId spielId = client.neuesSpiel("Vermerk");
+            final Halbzug halbzug = new Halbzug(new Position(E, _2), new Position(E, _4));
+            final Response resp1 = client.fuehreHalbzugAus(spielId.id, halbzug.toString());
+            Assert.assertEquals(201, resp1.getStatus());
+            // Spielbrett überprüfen:
+            final Response resp2 = client.spielbrett(spielId.id, "Tester", null);
+            final Spielbrett actual = resp2.readEntity(Spielbrett.class);
+            final Spielbrett expected = createInitialesSpielbrett().wendeHalbzugAn(halbzug);
+            Assert.assertEquals(expected, actual);
+
+        }
+        catch (SchachpartieApi.UngueltigeSpielIdException e) {
+            Assert.fail(e.toString());
+        }
+        catch (SchachpartieApi.UngueltigerHalbzugException e) {
+            Assert.fail(e.toString());
+        }
+    }
+
+    String[] UNSTERBLICHE_PARTY = {
+            "e2-e4", "e7-e5", "f2-f4", "e5-f4", "f1-c4", "d8-h4", "e1-f1", "b7-b5", "c4-b5",
+            "g8-f6", "g1-f3", "h4-h6", "d2-d3", "f6-h5", "f3-h4", "h6-g5", "h4-f5", "c7-c6",
+            "g2-g4", "h5-f6", "h1-g1", "c6-b5", "h2-h4", "g5-g6", "h4-h5", "g6-g5", "d1-f3",
+            "f6-g8", "c1-f4", "g5-f6", "b1-c3", "f8-c5", "c3-d5", "f6-b2", "f4-d6", "c5-g1",
+            "e4-e5", "b2-a1", "f1-e2", "b8-a6", "f5-g7", "e8-d8", "f3-f6", "g8-f6", "d6-e7"
+    };
+
+    String FINALES_SPIELBRETT =
+                    "-------------------------" +CR+
+                    "|Rb|  |Bb|Kb|  |  |  |Rb|" +CR+
+                    "-------------------------" +CR+
+                    "|Pb|  |  |Pb|Bw|Pb|Nw|Pb|" +CR+
+                    "-------------------------" +CR+
+                    "|Nb|  |  |  |  |Nb|  |  |" +CR+
+                    "-------------------------" +CR+
+                    "|  |Pb|  |Nw|Pw|  |  |Pw|" +CR+
+                    "-------------------------" +CR+
+                    "|  |  |  |  |  |  |Pw|  |" +CR+
+                    "-------------------------" +CR+
+                    "|  |  |  |Pw|  |  |  |  |" +CR+
+                    "-------------------------" +CR+
+                    "|Pw|  |Pw|  |Kw|  |  |  |" +CR+
+                    "-------------------------" +CR+
+                    "|Qb|  |  |  |  |  |Bb|  |" +CR+
+                    "-------------------------";
+
+
+    @Test
+    public void unsterblicheParty() {
+        System.out.println("Test: unsterblicheParty");
+        final RestServiceClient client = new RestServiceClient();
+        try {
+            final SpielId spielId = client.neuesSpiel("Vermerk");
+            for (String halbzugStr : UNSTERBLICHE_PARTY) {
+                final Halbzug halbzug = SpielNotationParser.parse(halbzugStr);
+                client.fuehreHalbzugAus(spielId.id, halbzug.toString());
+            }
+            final Halbzug halbzug = SpielNotationParser.parse("d8-c7");
+            try {
+                client.fuehreHalbzugAus(spielId.id, halbzug.toString());
+                Assert.fail("Expected UngueltigerHalbzugException not occured");
+            }
+            catch (SchachpartieApi.UngueltigerHalbzugException e) {
+                // expected case
+                Assert.assertEquals(Zugregel.DIE_PARTIE_ENDET_MATT, e.verletzteZugregel);
+            }
+            // Spielbrett überprüfen:
+            final Response resp2 = client.spielbrett(spielId.id, "Tester", null);
+            final Spielbrett actual = resp2.readEntity(Spielbrett.class);
+            Assert.assertEquals(FINALES_SPIELBRETT, actual.toString());
+        }
+        catch (Exception e) {
             Assert.fail(e.toString());
         }
     }
