a22e1fc294a5191eb2b074df587a7186918ef8c8
javacook <javacook@gmx.de>
2017-07-08 00:43:33 +0200

    initial

---
+++ src/test/java/com/iks/dddschach/domain/SpielbrettFactoryTest.java
@@ -0,0 +1,20 @@
+package com.iks.dddschach.domain;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+
+/**
+ * Created by vollmer on 08.07.17.
+ */
+public class SpielbrettFactoryTest {
+
+    @Test
+    public void decode() throws Exception {
+        final String spielbrett = "RNBQKBNRPPPPPPPP________________________________pppppppprnbqkbnr";
+        final Spielbrett decoded = SpielbrettFactory.decode(spielbrett);
+        Assert.assertEquals(spielbrett, decoded.encode());
+    }
+
+
+}
